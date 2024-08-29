b77585c5adc3574303a1b0a8e96833bd81bde940
Samuel Vary <samuelvary@samuels-air.mynetworksettings.com>
2024-08-29 13:55:32 -0400

    Login screens showing up correctly

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import firebase from '@react-native-firebase/app';
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp();
+}
 
 AppRegistry.registerComponent(appName, () => App);
